a53012459ed6cea5da507f6f1d054ad843ec366d
SakshiSrivastava55 <53784511+SakshiSrivastava55@users.noreply.github.com>
2020-12-11 12:45:32 +0530

    Update resolution.py

--- Lab_7/resolution.py
+++ Lab_7/resolution.py
@@ -1,105 +1,82 @@
 import re
-def getAttributes(expression):
-    expression = expression.split("(")[1:]
-    expression = "(".join(expression) 
-    expression = expression.split(")")[:-1]
-    expression = ")".join(expression)
-    attributes = expression.split(',')
-    return attributes
+def negate(term):
+    return f'~{term}' if term[0] != '~' else term[1]
 
-def getInitialPredicate(expression):
-    return expression.split("(")[0]
-def isConstant(char):
-    return char.isupper() and len(char) == 1
-
-def isVariable(char):
-    return char.islower() and len(char) == 1
-def replaceAttributes(exp, old, new):
-    attributes = getAttributes(exp)
-    predicate = getInitialPredicate(exp)
-    for index, val in enumerate(attributes):
-        if val == old:
-            attributes[index] = new
-    return predicate + "(" + ",".join(attributes) + ")"
-
-def apply(exp, substitutions):
-    for substitution in substitutions:
-        new, old = substitution  
-        exp = replaceAttributes(exp, old, new)
-    return exp
-def checkOccurs(var, exp):
-    if exp.find(var) == -1:
-        return False
-    return True
-
-
-def getFirstPart(expression):
-    attributes = getAttributes(expression)
-    return attributes[0]
-
-
-def getRemainingPart(expression):
-    predicate = getInitialPredicate(expression)
-    attributes = getAttributes(expression)
-    newExpression = predicate + "(" + ",".join(attributes[1:]) + ")"
-    return newExpression
-def unify(exp1, exp2):
-    if exp1 == exp2:
-        return []
-
-    if isConstant(exp1) and isConstant(exp2):
-        if exp1 != exp2:
-            print(f"{exp1} and {exp2} are constants. Cannot be unified")
-            return []
-
-    if isConstant(exp1):
-        return [(exp1, exp2)]
-
-    if isConstant(exp2):
-        return [(exp2, exp1)]
-
-    if isVariable(exp1):
-        return [(exp2, exp1)] if not checkOccurs(exp1, exp2) else []
-
-    if isVariable(exp2):
-        return [(exp1, exp2)] if not checkOccurs(exp2, exp1) else []
-
-    if getInitialPredicate(exp1) != getInitialPredicate(exp2):
-        print("Cannot be unified as the predicates do not match!")
-        return []
-
-    attributeCount1 = len(getAttributes(exp1))
-    attributeCount2 = len(getAttributes(exp2))
-    if attributeCount1 != attributeCount2:
-        print(f"Length of attributes {attributeCount1} and {attributeCount2} do not match. Cannot be unified")
-        return []
-
-    head1 = getFirstPart(exp1)
-    head2 = getFirstPart(exp2)
-    initialSubstitution = unify(head1, head2)
-    if not initialSubstitution:
-        return []
-    if attributeCount1 == 1:
-        return initialSubstitution
-
-    tail1 = getRemainingPart(exp1)
-    tail2 = getRemainingPart(exp2)
-
-    if initialSubstitution != []:
-        tail1 = apply(tail1, initialSubstitution)
-        tail2 = apply(tail2, initialSubstitution)
-
-    remainingSubstitution = unify(tail1, tail2)
-    if not remainingSubstitution:
-        return []
-
-    return initialSubstitution + remainingSubstitution
+def reverse(clause):
+    if len(clause) > 2:
+        t = split_terms(clause)
+        return f'{t[1]}v{t[0]}'
+    return ''
+def split_terms(rule):
+    exp = '(~*[PQRS])'
+    terms = re.findall(exp, rule)
+    return terms
+def contradiction(query, clause):
+    contradictions = [ f'{query}v{negate(query)}', f'{negate(query)}v{query}']
+    return clause in contradictions or reverse(clause) in contradictions
+def resolve(kb, query):
+    temp = kb.copy()
+    temp += [negate(query)]
+    steps = dict()
+    for rule in temp:
+        steps[rule] = 'Given.'
+    steps[negate(query)] = 'Negated conclusion.'
+    i = 0
+    while i < len(temp):
+        n = len(temp)
+        j = (i + 1) % n
+        clauses = []
+        while j != i:   
+            terms1 = split_terms(temp[i])
+            terms2 = split_terms(temp[j])
+            for c in terms1:
+                if negate(c) in terms2:
+                    t1 = [t for t in terms1 if t != c]
+                    t2 = [t for t in terms2 if t != negate(c)]
+                    gen = t1 + t2
+                    if len(gen) == 2:
+                        if gen[0] != negate(gen[1]):
+                            clauses += [f'{gen[0]}v{gen[1]}']
+                        else:
+                            if contradiction(query,f'{gen[0]}v{gen[1]}'):
+                                temp.append(f'{gen[0]}v{gen[1]}')
+                                steps[''] = f"Resolved {temp[i]} and {temp[j]} to {temp[-1]}, which is in turn null. \
+                                \nA contradiction is found when {negate(query)} is assumed as true. Hence, {query} is true."
+                                return steps
+                    elif len(gen) == 1:
+                        clauses += [f'{gen[0]}']
+                    else:
+                        if contradiction(query,f'{terms1[0]}v{terms2[0]}'):
+                            temp.append(f'{terms1[0]}v{terms2[0]}')
+                            steps[''] = f"Resolved {temp[i]} and {temp[j]} to {temp[-1]}, which is in turn null. \
+                            \nA contradiction is found when {negate(query)} is assumed as true. Hence, {query} is true."
+                            return steps
+            for clause in clauses:
+                if clause not in temp and clause != reverse(clause) and reverse(clause) not in temp:
+                    temp.append(clause)
+                    steps[clause] = f'Resolved from {temp[i]} and {temp[j]}.'
+            j = (j + 1) % n
+        i += 1
+    return steps
+def resolution(kb, query):
+    kb = kb.split(' ')
+    steps = resolve(kb, query)
+    print('\nStep\t|Clause\t|Derivation\t')
+    print('-' * 30)
+    i = 1
+    for step in steps:
+        print(f' {i}.\t| {step}\t| {steps[step]}\t')
+        i += 1
 def main():
-    print("Enter the first expression")
-    e1 = input()
-    print("Enter the second expression")
-    e2 = input()
-    substitutions = unify(e1, e2)
-    print("The substitutions are:")
-    print([' / '.join(substitution) for substitution in substitutions])
-main()                        
+    print("Enter the kb:")
+    kb = input()
+    print("Enter the query:")
+    query = input()
+    resolution(kb,query)
+#test 1
+#(P^Q)<=>R : (Rv~P)v(Rv~Q)^(~RvP)^(~RvQ)
+main()
+#test 2
+#(P=>Q)=>Q, (P=>P)=>R, (R=>S)=>~(S=>Q)
+main()
+                                
